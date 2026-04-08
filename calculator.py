ea59e955d88f9f7aac4fe8ee313932fa7d6d6f13
Cade Shapiro <redbeartrue@gmail.com>
2026-04-08 10:16:43 -0400

    modified calc

--- calculator.py
+++ calculator.py
@@ -13,3 +13,9 @@ def multiply(a, b): a * b
 def divide(a, b): b /a
 def logarithm(a, b): math.log(a, b)
 def exponent(a, b): a**b
+def add(a,b): a + b
+def sub(a,b): a - b
+def mul(a,b): a * b
+def div(a,b): b / a
+def log(a,b): math.log(a,b)
+def exp(a,b): a**b
