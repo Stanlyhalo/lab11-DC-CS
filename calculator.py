9104af3a6f54169e1e820276ea22f18a4482d598
Stanlyhalo <donttazemebro115@gmail.com>
2026-04-08 10:33:44 -0400

    Fixed?

--- calculator.py
+++ calculator.py
@@ -11,28 +11,13 @@ def add(a, b): return a + b
 def sub(a, b): return a - b
 def mul(a, b): return a * b
 def div(a, b):
-    result = 0
-    try:
-        if a == 0: raise ZeroDivisionError
-        result = b / a
-    except ZeroDivisionError:
-        result = 0
-    return result
+    if a == 0: raise ZeroDivisionError
+    return b / a
 def logarithm(a, b):
-    result = 0
-    try:
-        result = math.log(a, b)
-    except ValueError:
-        result = 0
-    return result
+    return math.log(a, b)
 def exp(a, b): return a**b
 def square_root(a):
-    result = 0
-    try:
-        if a < 0: raise ValueError
-        result = math.sqrt(a)
-    except ValueError:
-        result = 0
-    return result
+    if a < 0: raise ValueError
+    return math.sqrt(a)
 def hypotenuse(a, b):
     return math.hypot(a,b)
